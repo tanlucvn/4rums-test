09725e45be455eb7e92f4ada6219be48ad008565
Luke <93022030+tanluc1e@users.noreply.github.com>
2023-12-26 16:23:37 +0700

    Update index.jsx

--- client/src/routes/Auth/Login/index.jsx
+++ client/src/routes/Auth/Login/index.jsx
@@ -45,7 +45,7 @@ export default function Login() {
         setLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/auth/login', {
+            const response = await fetch('https://fourrums-server.onrender.com/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
